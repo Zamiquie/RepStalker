da861868705e48c00d84a78375a9b56246960c2d
Zamiquie <aymericbaquet@gmail.com>
2019-05-19 15:23:20 +0200

    ajout sur la branche dev

---
+++ TestXamarin/TestXamarin/Class/ApiGestion.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TestXamarin.Class
+{
+    public class ApiGestion
+    { 
+        public string Url { get; set; }
+        public int Port { get; set; }
+        public string Domaine { get; set; }
+        public RestClient Client { get; set; }
+
+        //Constructeur par default 
+        public ApiGestion()
+        {
+            if (Device.RuntimePlatform == Device.Android) Domaine = "10.0.0.2";
+            else Domaine = "localhost";
+
+            Port = 50565;
+            Url = "http://" + Domaine + ":" + Port.ToString() + "/api/projet/" ;
+            Client = new RestClient(Url);
+
+        }
+
+        public ApiGestion(string url)
+        {
+            Url = url;
+            Client = new RestClient(Url);
+        }
+
+        public List<Users> AllUser()
+        {
+            string ressource = "Users/AllUsers";
+            var request = new RestRequest(ressource,Method.GET);
+
+            //envoie request
+            request.OnBeforeDeserialization = resp => resp.ContentType = "application/json";
+            request.Timeout = 150;
+            IRestResponse reponse = Client.Execute(request);
+            var content = reponse.Content;
+            var Json = JsonConvert.DeserializeObject<List<Users>>(content);
+
+            return Json;
+        }
+
+        public List<OSMReturn> GetPositionByAdress(string ressource,string formatOutp,string address)
+        {
+            string uri = ressource + "?format="+formatOutp + "&q="+Uri.EscapeUriString(address);
+            var request = new RestRequest(uri, Method.GET);
+
+            //envoie request
+            request.OnBeforeDeserialization = resp => resp.ContentType = "application/json";
+            IRestResponse reponse = Client.Execute(request);
+            var content = reponse.Content;
+            var json = JsonConvert.DeserializeObject<List<OSMReturn>>(content);
+
+            return json;
+
+
+        }
+
+    }
+}
---
+++ TestXamarin/TestXamarin/NameRepertory.xaml.cs
@@ -0,0 +1,77 @@
+﻿using Plugin.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using TestXamarin.Class;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TestXamarin
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NameRepertory : ContentPage
+    {
+        
+        public ApiGestion Api { get; set; }
+
+        //Test en dur(voir en DB)
+        public List<Users> Users { get; set; }
+
+        public NameRepertory()
+        {
+            InitializeComponent();
+
+            Users = new List<Users>();
+            Api = new ApiGestion();
+
+            if (Api.AllUser() != null)
+            {
+                foreach (Users user in Api.AllUser())
+                {
+                    Users.Add(user);
+                }
+            }
+            else
+            {
+                foreach (var user in Plugin.ContactService.CrossContactService.Current.GetContactList().ToList())
+                {
+                    Users.Add(new Users()
+                    {
+                        Name = user.Name,
+                        Telephone = user.Number,
+                        Adress = "Le Buisson 38580 La chapelle du bard"
+                    });
+                    
+                }
+                Users = Users.OrderBy(m => m.Name).ToList();
+            }
+
+            MyListView.ItemsSource = Users;
+        }
+
+        async void Handle_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            if (e.Item == null)
+                return;
+            Users Personne = ((Users)e.Item);
+            var Display = await DisplayAlert(Personne.Name, Personne.Adress + "\n" + Personne.PostalCode + " " + Personne.City, "Appeler", "Rechercher sur la carte");
+
+            if (Display)
+            {
+                var PhoneCallTask = CrossMessaging.Current.PhoneDialer;
+                if (PhoneCallTask.CanMakePhoneCall) PhoneCallTask.MakePhoneCall(Personne.Telephone);
+            }
+            if (!Display)
+            {
+
+                await Navigation.PushAsync(new ContactMap(Personne));
+            }
+
+            //Deselect Item
+            ((ListView)sender).SelectedItem = null;
+        }
+    }
+}
